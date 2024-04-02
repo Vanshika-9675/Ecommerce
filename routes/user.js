09904e0c3ee1f350cb72980d839deb3107ef847a
Vanshika Sharma <85115951+Vanshika-9675@users.noreply.github.com>
2024-04-02 23:04:21 +0530

    Add files via upload

---
+++ routes/user.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+const { authenticateUser } = require('../controllers/middlewares');
+const {login, signup} = require("../controllers/auth");
+const {addProduct, getAllProducts} = require('../controllers/product');
+const {addOrder,getUserOrders} = require('../controllers/Order');
+const {addWish,getUserwishes,deleteWish} = require('../controllers/wishList');
+const {addToCart,fetchCartProducts,removeProduct,decreaseQuantity,increaseQuantity,removeAllProducts} = require('../controllers/cart')
+
+router.post("/login",login);
+router.post("/signup",signup);
+
+router.get('/product',getAllProducts);
+router.post('/product',addProduct);
+
+router.post('/order',authenticateUser,addOrder);
+router.get('/order',authenticateUser,getUserOrders)
+
+router.post('/wish',authenticateUser,addWish);
+router.get('/wish',authenticateUser,getUserwishes);
+router.delete('/wish/:_id',authenticateUser,deleteWish);
+
+router.post('/cart', authenticateUser, addToCart);
+router.get('/cart', authenticateUser, fetchCartProducts);
+router.delete('/cart', authenticateUser, removeAllProducts);
+router.delete('/cart/:productId', authenticateUser, removeProduct);
+router.put('/cart/decrease/:productId', authenticateUser, decreaseQuantity);
+router.put('/cart/increase/:productId', authenticateUser, increaseQuantity);
+
+module.exports = router;
